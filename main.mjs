1513c735729229215c70df3166c8788d78a0e715
zhenghaowen <705680835@qq.com>
2021-06-17 19:22:34 +0800

    modified:   main.mjs

--- main.mjs
+++ main.mjs
@@ -1,8 +1,7 @@
 import {getObjectsByPrototype, getRange} from '/game/utils';
 import {Creep, Flag, StructureTower} from '/game/prototypes';
 //import {StructureTower} from "/game/prototypes/tower";
-import {MOVE,ATTACK,TOUGH,HEAL,RANGED_ATTACK} from '/game/constants';
-import {} from '/arena';
+import {ATTACK, HEAL, MOVE, RANGED_ATTACK, TOUGH} from '/game/constants';
 
 global.resources={};
 global.elements={};
@@ -10,12 +9,14 @@ let App = {
     plugins: [
         resources_collect_plugin,
         element_collect_plugin,
+        commander_plugin,
         creep_guide_flag_plugin,
+        leader_elect_plugin,
         creep_attack_flag_plugin,
         tower_attack_plugin,
         creep_healthier_plugin,
         creep_ranged_attack_plugin,
-        creep_attack_plugin
+        creep_attack_plugin,
     ],
 
     run: function () {
@@ -23,6 +24,28 @@ let App = {
             p();
         }
     }
+};
+function commander_plugin() {
+    global.enemy = global.resources.towers[0].findClosestByRange(getObjectsByPrototype(Creep).filter(creep => !creep.my));
+}
+function leader_elect_plugin(){
+    let leader_seq = [TOUGH,RANGED_ATTACK,HEAL];
+
+    for (let l of leader_seq){
+        let tars=apply_creeps([{body_part_type:[l],amount:1}]);
+        if (tars.length>0){
+            for (let tar of tars){
+                if (tar.body.some(b=>b.type==="move")){
+                    global.leader = tar;
+                    console.log("leader:"+tar.id);
+                    return;
+                }
+            }
+
+        }
+    }
+
+
 }
 
 function resources_collect_plugin() {
@@ -42,6 +65,7 @@ function creep_guide_flag_plugin() {
     if (!guide_creep) return;
     let myFlag = global.elements.myFlag;
     for (let c of guide_creep) {
+        console.log("guide:"+c.id);
         c.moveTo(myFlag);
     }
 }
@@ -59,7 +83,11 @@ function creep_ranged_attack_plugin(){
     for(let range_attack of range_attackers){
         let closest_enemy = range_attack
             .findInRange(getObjectsByPrototype(Creep).filter(c => !c.my),3);
-        range_attack.rangedAttack(closest_enemy);
+
+        if (closest_enemy.length>0){
+            range_attack.rangedAttack(closest_enemy[0]);
+        }
+
     }
 
 }
@@ -67,17 +95,27 @@ function creep_ranged_attack_plugin(){
 function creep_attack_plugin(){
     let attackers = getObjectsByPrototype(Creep).filter(c=>c.my&&c.body.some(b=>b.type==="attack"));
     for(let attacker of attackers){
-        let closest_enemy = attacker
-            .findInRange(getObjectsByPrototype(Creep).filter(c => !c.my),1);
-        attacker.attack(closest_enemy);
+        let closest_enemy = attacker.findInRange(getObjectsByPrototype(Creep).filter(c => !c.my),1);
+        if (closest_enemy.length>0){
+            attacker.attack(closest_enemy[0]);
+        }
+
     }
 }
 function creep_attack_flag_plugin() {
+    let leader = global.leader;
+    if (!leader){
+        return;
+    }
+    let move_target = global.enemy?global.enemy:global.elements.enermyFlag;
+    if (global.resources.creeps.length<5 || leader.findInRange(getObjectsByPrototype(Creep).filter(c=>c.my),2).length>=5){
+        leader.moveTo(move_target);
+    }
+
     let attack_creeps = apply_resources( "creeps", 13);
     if (!attack_creeps) return;
-    let enemy_flag = global.elements.enermyFlag;
     for (let c of attack_creeps) {
-        c.moveTo(enemy_flag);
+        c.moveTo(leader);
 
     }
 }
@@ -87,6 +125,7 @@ function tower_attack_plugin() {
     if (!towers)return;
     let closest_enemy = towers[0]
         .findClosestByRange(getObjectsByPrototype(Creep).filter(c => !c.my));
+    if (!closest_enemy)return;
     for (let t of towers){
         if (getRange(t,closest_enemy)<10){
             t.attack(closest_enemy);
@@ -96,7 +135,8 @@ function tower_attack_plugin() {
 }
 
 function apply_resources(type, amount) {
-    if (!global.resources[type] || global.resources[type].length < amount) {
+    amount = Math.min(global.resources[type].length,amount);
+    if (!global.resources[type] || amount === 0) {
         return null;
     } else {
         let result = global.resources[type].slice(0, amount);
@@ -115,10 +155,10 @@ function apply_creeps(opts){
     let res = [];
     for (let opt of opts){
         for (let c of global.resources.creeps){
-            let satisfy = false;
+            let satisfy = true;
             for (let bp of opt.body_part_type){
-                if (c.body.some(b=>b.type===bp)){
-                    satisfy=true;
+                if (c.body.every(b=>b.type!==bp)){
+                    satisfy=false;
                     break;
                 }
             }
